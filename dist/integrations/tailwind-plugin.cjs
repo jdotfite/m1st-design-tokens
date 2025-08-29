fa9ee441f7a99be3ca70cce109a1251287f4e9a9
Fite <FiteJ@members1st.org>
2025-08-29 16:09:06 -0400

    release: 2.3.7 with rebuilt dist and correct package version

--- dist/integrations/tailwind-plugin.cjs
+++ dist/integrations/tailwind-plugin.cjs
@@ -1,4 +1,4 @@
-/** Generated 2025-08-29T19:51:21.279Z */
+/** Generated 2025-08-29T20:09:00.617Z */
 module.exports = function({ addUtilities }) {
   const utilities = {
     '.heading-xxl': { fontFamily: 'var(--font-family-heading)', fontSize: 'var(--font-size-heading-xxl)', fontWeight: 'var(--font-weight-bold)', lineHeight: '1.1', letterSpacing: 'var(--font-letter-spacing-tight)' },
